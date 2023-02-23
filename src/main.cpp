e4329b02bdcf0fbbd331dc27865e28cf3f54a743
JossKM <jossmooyoung@gmail.com>
2023-02-23 11:24:28 -0500

    Lab 7-1: Sprite class

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include <Windows.h>
 #include <SDL.h> // Allows us to use features from the SDL Library
 #include <SDL_Image.h>
-#include "main.h"
 
 /*
 Let's use SDL to open a window, and render some sprites at given locations and scales
@@ -20,9 +19,61 @@ SDL_Window* pWindow = nullptr; //This is a pointer to SDL_Window. It stores a me
 SDL_Renderer* pRenderer = nullptr;
 bool isGameRunning = true;
 
-SDL_Texture* pMySprite = nullptr; //Texture is another word for image in game programming
-SDL_Rect mySpriteSrc;
-SDL_Rect mySpriteDst;
+namespace Fund
+{
+	//Declaring a struct or class declares a new type of object we can make. After this is declared, we can make Sprite variables that
+	//have all of the contained data fields, and functions
+	struct Sprite
+	{
+	public:
+		//public fields can be accessed from outside the struct or class
+		SDL_Texture* pTexture;
+		SDL_Rect src;
+		SDL_Rect dst;
+		double rotation = 0;
+		SDL_RendererFlip flipState = SDL_FLIP_NONE;
+
+		//This is a constructor. This is a special type of function used when creating an object.
+		//The compiler knows it's a constructor because it has parentheses like a function, 
+		//has the SAME NAME as the struct or class, and has no return type. 
+		//This one has no arguments. In that case it's called the Default Constructor and is used to set default values
+		Sprite()
+		{
+			std::cout << "Sprite default constructor\n";
+			pTexture = nullptr;
+			src = SDL_Rect{ 0,0,0,0 };
+			dst = SDL_Rect{ 0,0,0,0 };
+		}
+
+		//Constructors can have arguments as well, which is handy when we need to make them different
+		Sprite(SDL_Renderer* renderer, const char* filePathToLoad)
+		{
+			std::cout << "Sprite filepath constructor\n";
+;			src = SDL_Rect{ 0,0,0,0 };
+			pTexture = IMG_LoadTexture(renderer, filePathToLoad); //load into our pTexture pointer
+			if (pTexture == NULL) // if failed load
+			{
+				std::cout << "Image failed to load: " << SDL_GetError() << std::endl;
+			}
+			SDL_QueryTexture(pTexture, NULL, NULL, &src.w, &src.h); //ask for the dimensions of the texture
+			dst = SDL_Rect{ 0,0,src.w,src.h};
+			//At this point, the width and the height of the texture should be placed at the memory addresses of src.w and src.h
+		}
+
+		//This draw function can be called on individual variables of type Fund::Sprite, 
+		//which will use their own variables to call SDL_RenderCopy. So, we can declare and draw new sprites with two lines:
+		//Fund::Sprite myNewSprite = Sprite(pRenderer, "somefile.png");
+		//myNewSprite.Draw(pRenderer);
+		void Draw(SDL_Renderer* renderer)
+		{
+			SDL_RenderCopyEx(renderer, pTexture, &src, &dst, rotation, NULL, flipState);
+		}
+	};
+}
+
+Fund::Sprite spriteStarshipEnterprise;
+Fund::Sprite klingonShip1;
+Fund::Sprite klingonShip2;
 
 //Initialize SDL, open a window and set up renderer. Returns false if failed
 bool Init()
@@ -68,27 +119,24 @@ bool Init()
 void Load()
 {
 	char* fileToLoad = "../Assets/textures/enterprise.png";
-	//SDL_Texture * IMG_LoadTexture(SDL_Renderer *renderer, const char *file);
-	pMySprite = IMG_LoadTexture(pRenderer, fileToLoad);
-	if (pMySprite == NULL)
-	{
-		std::cout << "Image failed to load: " << fileToLoad;
-	}
 
-	//Describe location to copy from in the texture
-	mySpriteSrc.x = 0;
-	mySpriteSrc.y = 0;
-	mySpriteSrc.w = 643;
-	mySpriteSrc.h = 296;
+	spriteStarshipEnterprise = Fund::Sprite(pRenderer, fileToLoad);
 
-	int shipWidth = mySpriteSrc.w / 4;
-	int shipHeight = mySpriteSrc.h / 4;
+	int shipWidth = spriteStarshipEnterprise.src.w / 4;
+	int shipHeight = spriteStarshipEnterprise.src.h / 4;
 
 	//Describe location to paste to on the screen
-	mySpriteDst.w = shipWidth;
-	mySpriteDst.h = shipHeight;
-	mySpriteDst.x = 50;
-	mySpriteDst.y = (SCREEN_HEIGHT / 2) - (shipHeight / 2);
+	spriteStarshipEnterprise.dst.w = shipWidth;
+	spriteStarshipEnterprise.dst.h = shipHeight;
+	spriteStarshipEnterprise.dst.x = 50;
+	spriteStarshipEnterprise.dst.y = (SCREEN_HEIGHT / 2) - (shipHeight / 2);
+
+
+	klingonShip1 = Fund::Sprite(pRenderer, "../Assets/textures/d7_small.png");
+	klingonShip1.dst.x = 500;
+	klingonShip1.dst.y = 300;
+	klingonShip1.flipState = SDL_FLIP_HORIZONTAL;
+	klingonShip1.rotation = 10.0;
 }
 
 void Input()
@@ -98,22 +146,17 @@ void Input()
 
 void Update()
 {
-	mySpriteDst.x += 2;
+	spriteStarshipEnterprise.dst.x += 2;
 }
 
 void Draw()
 {
-		/*
-		* Will use...
-		* int SDL_RenderCopy(SDL_Renderer * renderer,
-                   SDL_Texture * texture,
-                   const SDL_Rect * srcrect,
-                   const SDL_Rect * dstrect);
-		*/
 	SDL_SetRenderDrawColor(pRenderer, 5, 5, 15, 255);
 	SDL_RenderClear(pRenderer);
 
-	SDL_RenderCopy(pRenderer, pMySprite, &mySpriteSrc, &mySpriteDst);
+	spriteStarshipEnterprise.Draw(pRenderer);
+	klingonShip1.Draw(pRenderer);
+
 	// RenderPresent shows the hidden space we were drawing to called the BackBuffer. 
 	// For more information why we use this, look up Double Buffering Rendering
 	SDL_RenderPresent(pRenderer);
